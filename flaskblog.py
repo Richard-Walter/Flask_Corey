09269302fdc25970b28115deeb2d25e5393a51e4
Richard-Walter <willthisnamedo@gmail.com>
2020-08-15 16:03:29 +1000

    add registration and login

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,11 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+from flask import flash
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '100d56df75a29ea6717b1db3436e06b8'   # prevents website attacks, cookie manipulation
 
 posts = [
     {
@@ -30,6 +34,27 @@ def home():
 def about():
     return render_template("about.html", title='About')
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+
+    return render_template("register.html", title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful!', 'danger')
+    return render_template("login.html", title='Login', form=form) 
+        
 
 if __name__ == "__main__":
 
